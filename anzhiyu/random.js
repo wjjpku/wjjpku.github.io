f03f20bfb7c1e5b35cdb7a9b7fb41f6204022e7f
wjj1234321 <wujiaju4@gmail.com>
2025-07-01 04:40:40 +0800

    Site updated: 2025-07-01 04:40:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/25/hello-world/","2025/06/29/post/"];function toRandomPost(){
+var posts=["2025/06/29/first_post/","2025/06/26/书摊/","2025/06/11/毕业遗憾/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
