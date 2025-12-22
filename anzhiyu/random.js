7c60dec1f764429743cbb68bf09517b7215bf787
wjjpku <wujiaju4@gmail.com>
2025-12-22 14:44:52 +0800

    Site updated: 2025-12-22 14:44:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/11/毕业遗憾/","2025/06/26/书摊/","2025/06/29/first_post/"];function toRandomPost(){
+var posts=["2025/06/11/毕业遗憾/","2025/06/26/书摊/","2025/12/21/love/","2025/09/11/competition/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
